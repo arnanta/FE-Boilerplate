974a9e29f35de03f1fb8e9b9fbfeb10601aa890c
Arnanta Chatterjee <arnantachatterjee000@gmail.com>
2025-08-15 23:08:13 +0530

    fix: fixed tailwind config with vite and added card from shad

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import Inspect from 'vite-plugin-inspect';
 import { visualizer } from 'rollup-plugin-visualizer';
+import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     Inspect({
       build: true,
       outputDir: '.vite-inspect',
